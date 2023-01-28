3d535533b9bf3b517e0a07bd5c3b76a8e967b657
cas1m1r <tyl3r5durd3n@gmail.com>
2023-01-28 02:27:14 -0500

    fixes

--- lumberjack.py
+++ lumberjack.py
@@ -1,4 +1,4 @@
-import test_case
+from heuristics import Defender
 from nodes import *
 import json
 import ast
@@ -7,50 +7,69 @@ import os
 
 
 def read_tree(pycode: bytes):
-    parsed = {'imports': [], 'functions': [], 'classes': [], 'libraries': []}
+    parsed = {'imports': [], 'functions': [], 'classes': [], 'libraries': [], 'if_main': []}
     tree = ast.parse(pycode)
     body = tree.body
+    imports = []
     for element in body:
         # check for import statements
         if type(element) == ast.Import:
-            parsed['imports'].append(parse_import(element))
+            imports.append(parse_import(element))
             for lib in element.names:
                 parsed['libraries'].append(lib.name)
         # check for individual module imports
         elif type(element) == ast.ImportFrom:
-            parsed['imports'].append(parse_import_from(element))
+            imports.append(parse_import_from(element))
             parsed['libraries'].append(element.module)
         # check for function definitions
         elif type(element) == ast.FunctionDef:
-            parsed['functions'].append(parse_function_def(element))
-
+            fsig, data = parse_function_def(element)
+            parsed['functions'].append(fsig)
+            # TODO: feed content of code in data variable to heuristics
         # check for class definitions
         elif type(element) == ast.ClassDef:
-            parsed['classes'].append(explore_class(element))
+            csig, data = explore_class(element)
+            # TODO: feed content of code in data variable to heuristics
+            parsed['classes'].append(csig)
         # look for conditionals (will likely be __name__ == '__main__' at this level)
         elif type(element) == ast.If:
             parsed['if_main'].append(parse_if_main(element))
+    for ln in imports:
+        for i in ln: 
+            parsed['imports'].append(i)
     return parsed
 
 
 def explore_class(c: ast.ClassDef):
     data = {'name': c.name, 'functions': []}
+    content = {}
     for node in c.body:
         # look for function definitions
         if type(node) == ast.FunctionDef:
-            data['functions'].append(str(parse_function_def(node)))
-    return data
+            fsig, lines = parse_function_def(node)
+            content[fsig] = lines
+            data['functions'].append(str(fsig))
+    return data, content
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
+        file_in = 'definitely_malicious.py'
         # from test_case import *
         # test_case.FakeDataGenerator('testData')
-        code = open('test_case.py', 'rb').read()
+        code = open(file_in, 'rb').read()
     elif sys.argv[1].split('.')[-1] != 'py':
         print('[!] Please give lumberjack a python file :(')
         exit()
     else:
-        code = open(sys.argv[1], 'rb').read()
+        file_in = sys.argv[1]
+        code = open(file_in, 'rb').read()
     # parse and show the summary of the code
-    print(json.dumps(read_tree(code), indent=2))
+    tree = read_tree(code)
+    tree['warnings'] = Defender(tree).warnings
+    # dump results
+    filename = f"{file_in.split('.')[0]}_summary.json"
+    if len(tree['warnings']):
+        for warning in tree['warnings']:
+            print(f'[{file_in}]: {warning}')
+    open(filename, 'w').write((json.dumps(tree, indent=2)))
--- nodes.py
+++ nodes.py
@@ -7,15 +7,20 @@ def parse_args(node: ast.arguments):
         fields = a._fields
         if 'annotation' in fields and a.annotation != None:
             results.append(f'{a.arg}: {a.annotation.id}')
-        else:
+        elif 'arg' in fields:
             results.append(a.arg)
+        elif 'id' in fields:
+            results.append(a.id)
+        else:
+            results.append(a.value)
     return results
 
 
 def parse_body(body: list):
     elements = []
     actions = {ast.arguments: parse_args, ast.FunctionDef: parse_function_def,
-               ast.Import: parse_import, ast.ImportFrom: parse_import_from}
+               ast.Import: parse_import, ast.ImportFrom: parse_import_from,
+               ast.For: parse_for_loop}
     for node in body:
         T = type(node)
         if T not in actions.keys():
@@ -28,15 +33,15 @@ def parse_body(body: list):
 def parse_function_def(f:ast.FunctionDef):
     args = parse_args(f.args)
     fcn = f'{f.name}({", ".join(args)})'
-    # TODO: parse body
-    return fcn
+    body = expand_body(f.body)
+    return fcn, body
 
 
 def parse_import(i: ast.Import):
-    imp = f''
+    imps = []
     for lib in i.names:
-        imp += f'import {lib.name}'
-    return imp
+        imps.append(lib.name)
+    return imps
 
 
 def parse_import_from(i:ast.ImportFrom):
@@ -44,39 +49,96 @@ def parse_import_from(i:ast.ImportFrom):
     imports = []
     for lib in i.names:
         imports.append(lib.name)
-    return f'from {m} import {",".join(imports)}'
+    return [m, imports]
 
 
 def parse_if_main(i: ast.If):
     # TODO: verify this is an if '__name__' == '__main__' statement
-    routine = {'body': parse_body(i.body)}
-    # TODO: determine entry point to main
+    routine = {'body': expand_body(i.body)}
     return routine
 
 
-
-# TODO: Generic parsing of node types:
-def walk_node(node):
-    data = {'name': '', 'args': '','actions': [], 'type':''}
-    types = {ast.Import: 'import', ast.ImportFrom: 'import from',
-             ast.Call: ''}
-    if type(node) in types.keys():
-        data['type'] = types[type(node)]
-    for branch in ast.walk(node):
-        fields = branch._fields
-        if 'func' in fields:
-            data['actions'].append(branch.func.id)
-        if 'name' in fields:
-            data['name'] = branch.name
-        if 'args' in fields:
-            data['args'] = parse_args(branch)
-        if 'value' in fields:
-            if type(branch.value) == ast.Constant:
-                data['val'] = branch.value.value
+def parse_if(node: ast.If):
+    ops = {ast.In: ' in ', ast.NotIn: ' not in ', ast.Eq: ' == ', ast.NotEq: ' != ',
+           ast.And: ' and '}
+    data = ''
+    if 'left' in node.test._fields:
+        lhs = node.test.left
+        test = ''
+        for v in node.test.ops:
+            t = type(v)
+            if t in ops.keys():
+                test += ops[t]
+        for op in node.test.comparators:
+            T = type(op)
+            if T == ast.Call:
+                rhs = '.'.join([op.func.value.id, op.func.attr])
+                data = f'if {lhs.id} {test} {rhs}()'
             else:
-                data['val'] = branch.value.id
+                print(f'Havent created if {T}')
+    elif type(node.test) != ast.Call and type(node.test.op) in ops.keys():
+        data = ops[type(node.test.op)]
     return data
 
 
+def expand_body(body:list):
+    data = {'code': []}
+    actions = {ast.arguments: parse_args, ast.FunctionDef: parse_function_def,
+               ast.Import: parse_import, ast.ImportFrom: parse_import_from,
+               ast.Expr: expand_expr, ast.If: parse_if, ast.Try: expand_try,
+               ast.For: parse_for_loop}
+    for node in body:
+        T = type(node)
+        if T in actions.keys():
+            data['code'].append(actions[T](node))
+        else:
+            print(f'No Method for {T}')
+    return data
 
 
+def expand_expr(e: ast.Expr):
+    if 'value' in e.value._fields:
+        return e.value.value
+
+
+def expand_try(t: ast.Try):
+    data = {'try': expand_body(t.body),
+            'catch': expand_body(t.handlers)}
+    return data
+
+
+def parse_for_loop(f: ast.For):
+    data = {}
+    loopvar = f.target.id
+    iterable = f.iter
+    data['loop_element'] = loopvar
+    # parse the iterable portion of forloop
+    if type(f.iter) == ast.Call:
+        if 'attr' in f.iter.func._fields and 'id' in f.iter.func.value._fields:
+            able = f'{".".join([f.iter.func.value.id, f.iter.func.attr])}('
+        elif type(f.iter.func) == ast.Attribute:
+            able = f'{unpack_attribute(f.iter.func)}('
+        else:
+            able = f'{f.iter.func.id}('
+        data['iterable'] = able
+        for arg in f.iter.args:
+            if type(arg) == ast.Constant:
+                data['iterable'] = f'{arg.value}'
+            elif 'attr' in arg._fields:
+                data['iterable'] += f'{arg.attr}, '
+            elif type(arg) == ast.Call:
+                data['iterable'] += f'{parse_args(arg)}, '
+        data['iterable'] += ')'
+    return data
+
+
+def unpack_attribute(a : ast.Attribute):
+    attr = []
+    for level in ast.walk(a):
+            items = level._fields
+            if 'attr' in items:
+                attr.append(level.attr)
+            elif 'id' in items:
+                attr.append(level.id)
+    attr.reverse()
+    return '.'.join(attr)
